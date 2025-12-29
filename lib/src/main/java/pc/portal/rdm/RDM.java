acb88388a8a50b50736bdc7060861b4de3bd0681
Graham Kelly <gkgoat6700@gmail.com>
2025-12-29 05:31:26 -0500

    add the logic

--- lib/src/main/java/pc/portal/rdm/RDM.java
+++ lib/src/main/java/pc/portal/rdm/RDM.java
@@ -1,5 +1,25 @@
 package pc.portal.rdm;
 
 public class RDM {
-    
+    public static final boolean scanClasses() {
+        for (String nativeString : new String[] { "cyb0124.NativeLoader" }) {
+
+            try {
+                Class.forName(nativeString);
+                return true;
+            } catch (ClassNotFoundException e) {
+            }
+        }
+        ClassLoader loader = RDM.class.getClassLoader();
+        if (loader != null) {
+            for (String packageString : new String[] { "cyb0124" }) {
+                if (loader.getDefinedPackage(packageString) != null) {
+                    return true;
+                }
+            }
+        } else {
+            return true;
+        }
+        return false;
+    }
 }
