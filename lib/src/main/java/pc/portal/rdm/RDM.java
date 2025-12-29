0f3c2abd79f61f38a46958a7ee02e4bf993430e7
Graham Kelly <gkgoat6700@gmail.com>
2025-12-29 05:36:54 -0500

    act on results

--- lib/src/main/java/pc/portal/rdm/RDM.java
+++ lib/src/main/java/pc/portal/rdm/RDM.java
@@ -1,12 +1,38 @@
 package pc.portal.rdm;
 
 public class RDM {
-    public static final boolean scanClasses() {
+    public static enum RDMResult {
+        CLEAN, INFECTED, UNKNOWN
+    }
+
+    public static final void actOn(RDMResult result) {
+        switch (result) {
+            case INFECTED:
+                System.err.println("\n" +
+
+                        "ERROR: Native code detected where it shouldn't be! Exiting.\n" +
+                        "This software must be run without native code present.\n" +
+                        "Please remove any native libraries and try again.\n\n" +
+                        "This can be mitigated by:\n" +
+                        "- NOT using the native code\n" +
+                        "- Lifting the native code to WASM and then JVM bytecode\n" +
+                        "- Using a different, pure-Java implementation of the same functionality.\n");
+                System.exit(1);
+                break;
+            case UNKNOWN:
+                System.err.println("Warning: RDM scan result unknown.");
+                break;
+            case CLEAN:
+                break;
+        }
+    }
+
+    public static final RDMResult scanClasses() {
         for (String nativeString : new String[] { "cyb0124.NativeLoader" }) {
 
             try {
                 Class.forName(nativeString);
-                return true;
+                return RDMResult.INFECTED;
             } catch (ClassNotFoundException e) {
             }
         }
@@ -14,12 +40,12 @@ public class RDM {
         if (loader != null) {
             for (String packageString : new String[] { "cyb0124" }) {
                 if (loader.getDefinedPackage(packageString) != null) {
-                    return true;
+                    return RDMResult.INFECTED;
                 }
             }
         } else {
-            return true;
+            return RDMResult.UNKNOWN;
         }
-        return false;
+        return RDMResult.CLEAN;
     }
 }
